3ebbafca70596cc4fcc29f865dd3fdb2ac5bc7c3
xiaojie <xiaojieonly@foxmail.com>
2024-04-08 10:35:00 +0800

    update

--- main.py
+++ main.py
@@ -11,14 +11,24 @@ import base64
 import hashlib
 from bs4 import BeautifulSoup
 
+TRANSLATION_PATCH = {
+    # 'l:chinese': '汉语'
+}
+
 def removeEmojis(x):
     return ''.join(c for c in x if c <= '\uFFFF')
 
-def parseMarkdownFile(path):
+def fixTranslation(x, y):
+    if x in TRANSLATION_PATCH:
+        return x, TRANSLATION_PATCH[x]
+    else:
+        return x, y
+
+def parseMarkdownFile(path, prefix):
     f = open(path, 'r', encoding='utf-8')
     html = markdown.markdown(f.read(), extensions=['markdown.extensions.tables'])
 
-    result = {}
+    result = []
     soup = BeautifulSoup(html, 'html.parser')
     for tr in soup.find_all('tr'):
         tds = [x for x in tr.find_all('td')]
@@ -27,7 +37,17 @@ def parseMarkdownFile(path):
             y = ''.join(tds[1].strings).strip()
             y = removeEmojis(y)
             if len(x) != 0 and len(y) != 0:
-                result[x] = y
+                result.append((x, y))
+
+    # Check result
+    bad = [x[0] for x in result if not re.fullmatch(r'[a-z0-9.\-\u0020]+', x[0])]
+    if bad:
+        raise ValueError('Bad tags', str(bad))
+
+    # Add prefix
+    if prefix:
+        p = prefix + ':'
+        result = [fixTranslation(p + x[0], x[1]) for x in result]
 
     return result
 
@@ -39,14 +59,28 @@ def sha1(path):
             if not data:
                 break
             sha1.update(data)
-    return sha1.hexdigest()
+    return sha1.digest()
 
 def saveTags(path, tags):
-    with open(path, 'w') as f:
-        json.dump(tags, f, ensure_ascii=False, indent=0)
+    tags = sorted(tags)
+    with open(path, 'wb') as f:
+        # write size placeholder
+        f.write(struct.pack('>i', 0))
+        # write tags
+        for x, y in tags:
+            f.write(x.encode())
+            f.write(struct.pack('b', ord('\r')))
+            f.write(base64.b64encode(y.encode()))
+            f.write(struct.pack('b', ord('\n')))
+        # get file size
+        f.seek(0, 2)
+        size = f.tell()
+        # write tags size
+        f.seek(0, 0)
+        f.write(struct.pack('>i', size - 4))
 
     # Save sha1
-    with open(path + ".sha1", 'w') as f:
+    with open(path + ".sha1", 'wb') as f:
         f.write(sha1(path))
 
 def downloadMarkdownFiles():
@@ -71,7 +105,6 @@ if __name__ == "__main__":
         removeMarkdownFiles()
     downloadMarkdownFiles()
 
-    tags = {}
     files = (
         ('rows.md', 'n'),
         ('artist.md', 'a'),
@@ -86,7 +119,7 @@ if __name__ == "__main__":
         ('parody.md', 'p'),
         ('reclass.md', 'r')
     )
-    for f, p in files: tags[p] = parseMarkdownFile(os.path.join('Database', 'database', f))
+    tags = [x for f, p in files for x in parseMarkdownFile(os.path.join('Database', 'database', f), p)]
     saveTags('tag-translations/tag-translations-zh-rCN.json', tags)
 
     removeMarkdownFiles()
